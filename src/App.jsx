1e0aebf10bce54a495abdbee0211b53643ce5364
Francisco <paco.jeansl.gameplay2@gmail.com>
2026-03-19 18:11:45 -0600

    menu3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Menu2 from "./pages/Menu2";
+import Menu3 from "./pages/Menu3";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/menu2" element={<Menu2 />} />
+        <Route path="/menu3" element={<Menu3 />} />
       </Routes>
     </BrowserRouter>
   );
